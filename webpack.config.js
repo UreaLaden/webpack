7737e26d154e2d0e283d87413b8f897bdb1b29c6
Leaundrae Mckinney <pytechdrae@gmail.com>
2023-04-25 13:10:39 -0400

    Playing webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,81 @@
-const path = require('path'); //Cannot use ECMA script modules in this file
+const path = require("path"); //Cannot use ECMA script modules in this file
+const TerserPlugin = require("terser-webpack-plugin"); //Installed with webpack5
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname,'./dist'),
-        publicPath:'dist/'
-        //publicPath:'auto' //This happens behind the scenes with Webpack5
-    },
-    mode: 'none',
-    module:{
-        rules:[
-            {
-                test:/\.(png|jpg)$/, //Does the file contain png or jpg
-                type: 'asset', //asset/resource, asset/inline,asset/source,asset
-                parser:{
-                    dataUrlCondition:{ // asset/inline < 3 > asset/resource
-                        maxSize: 3 * 1024 // 3 kilobytes
-                    }
-                }
-            },
-            {
-                test:/\.txt/,
-                type:'asset/source' // Webpack reads text file in as js string
-            },
-            {
-                test:/\.css$/,
-                use:[
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test:/\.scss$/,
-                use:[ // Webpack process loaders from right to left. Order matters
-                    'style-loader','css-loader','sass-loader'
-                ]
-            },
-            {
-                test:/\.js$/,
-                exclude:/node_modules/, //Except those located in this folder
-                use:{
-                    loader:'babel-loader',
-                    options:{
-                        presets:['@babel/env'],
-                        plugins:['@babel/plugin-proposal-class-properties']
-                    }
-                }
-            }
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[contenthash].js", //Updates on changes only
+    path: path.resolve(__dirname, "./dist"),
+    //publicPath:'auto' //This happens behind the scenes with Webpack5
+    publicPath: "",
+    // clean:{ These are the only two properties
+    //     dry:true, //webpack tells us which files will be removed
+    //     keep: /\.css/ //tells webpack which files to keep
+    // }
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/, //Does the file contain png or jpg
+        type: "asset", //asset/resource, asset/inline,asset/source,asset
+        parser: {
+          dataUrlCondition: {
+            // asset/inline < 3 > asset/resource
+            maxSize: 3 * 1024, // 3 kilobytes
+          },
+        },
+      },
+      {
+        test: /\.txt/,
+        type: "asset/source", // Webpack reads text file in as js string
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // Webpack process loaders from right to left. Order matters
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, //Except those located in this folder
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+      {
+        test: /\.hbs$/,
+        use:[
+            'handlebars-loader'
         ]
-    }
+      }
+    ],
+  },
+  plugins: [
+    new TerserPlugin(), // Makes Bundle smaller
+    new MiniCssExtractPlugin({
+      //Extract all css into a separate css file
+      filename: "styles.[contenthash].css",
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ 
+      title: "Hello world",
+      template: "src/index.hbs",
+      description: "Some description",
+    }),
+  ],
 };
